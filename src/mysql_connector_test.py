5c08db41c218cbf301f24b65db77a2b6fc32bfcf
Shinjiro Sakaitani <ssakaitani@gmail.com>
2020-10-29 13:20:06 +0900

    Add argument parser

--- src/mysql_connector_test.py
+++ src/mysql_connector_test.py
@@ -1,6 +1,19 @@
+from argparse import ArgumentParser
+
 from mysql.connector import connect
 
 
-if __name__ == '__main__':
-    with connect(host='192.168.33.10', user='user', password='password') as conn:
+def connect_db(host, user, password):
+    with connect(host=host, user=user, password=password) as conn:
         print('Connection Successful.')
+
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('host')
+
+    args = parser.parse_args()
+
+    connect_db(args.host, 'user', 'password')
+
